2af0864e9a6e247fb39507db23c19d424d0c0208
C4rlosDcJ <root@MacBook-Air-de-CarlosDc.local>
2025-04-08 01:47:49 -0600

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,41 +1,25 @@
 <?php
 
-namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
-use App\Models\User;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        $user = new User;
-        $user->name = 'Admin';
-        $user->email = 'admin@test.com';
-        $user->role = 'Administrador';
-        $user->password = '1234';
+        // Crear los roles
+        $adminRole = Role::create(['name' => 'Administrador']);
+        $doctorRole = Role::create(['name' => 'Doctor']);
+        $pacienteRole = Role::create(['name' => 'Paciente']);
 
-        $user->save();
-
-        $user1 = new User;
-        $user1->name = 'Doctor';
-        $user1->email = 'doc@test.com';
-        $user1->role = 'Doctor';
-        $user1->password = '1234';
-
-        $user1->save();
-
-        $user2 = new User;
-        $user2->name = 'Paciente';
-        $user2->email = 'paciente@test.com';
-        $user2->role = 'Paciente';
-        $user2->password = '1234';
-
-        $user2->save();
+        // Crear un usuario y asignarle el rol de 'Administrador'
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@hospital.com',
+            'password' => Hash::make('1234567890.'),  // Contraseña cifrada
+            'role_id' => $adminRole->id,  // Asignar el rol
+        ]);
     }
 }
